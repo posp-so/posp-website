d903b049e9b30cfda97f0d2e3d8975ea3308b29b
Zach Blake <ztb@pm.me>
2022-03-05 23:48:39 -0800

    wip: navigation bar. need to create more permanent logo

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import logo from '../public/vercel.svg'
 import discordLogo from '../public/discord.svg'
 import twitterLogo from '../public/twitter.svg'
+import githubLogo from '../public/github.svg'
 import BrandButton from './BrandButton'
 import NavLink from './NavLink'
 
@@ -19,7 +20,7 @@ const MenuBar: React.FC = () => {
           <div className="mx-auto max-w-7xl px-2">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -43,7 +44,7 @@ const MenuBar: React.FC = () => {
               </div>
               <div className="absolute inset-y-0 right-0 hidden items-center space-x-6 pr-2 sm:static sm:inset-auto sm:ml-6 sm:flex sm:pr-0">
                 <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex items-center justify-center space-x-4 align-middle font-mono">
+                  <div className="flex items-center justify-center space-x-8 align-middle font-mono">
                     <NavLink to="https://careers.posp.so">Careers</NavLink>
                     <NavLink to="https://discord.gg/XH66BZFW4m">
                       <Image
@@ -63,6 +64,15 @@ const MenuBar: React.FC = () => {
                       />
                       <span className="ml-2">Twitter</span>
                     </NavLink>
+                    <NavLink to="https://github.com/posp-so">
+                      <Image
+                        src={githubLogo}
+                        alt="GitHub Logo"
+                        width={18}
+                        height={18}
+                      />
+                      <span className="ml-2">GitHub</span>
+                    </NavLink>
                     <Link href="https://app.posp.so">
                       <a>
                         <BrandButton>Go to app</BrandButton>
@@ -79,44 +89,50 @@ const MenuBar: React.FC = () => {
             enter="transition duration-100 ease-out"
             enterFrom="transform scale-95 opacity-0"
             enterTo="transform scale-100 opacity-100"
-            leave="transition duration-75 ease-out"
+            leave="transition duration-50 ease-out"
             leaveFrom="transform scale-100 opacity-100"
             leaveTo="transform scale-95 opacity-0"
           >
             <Disclosure.Panel className="w-full sm:hidden">
-              <div className="mx-auto flex h-screen w-full flex-col space-y-6 bg-slate-800 px-2">
+              <div className="mx-auto flex h-full w-full flex-col space-y-6 bg-white px-2">
                 <Disclosure.Button>
-                  <Link href="#about">
-                    <a className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
-                      About
-                    </a>
-                  </Link>
+                  <NavLink to="https://careers.posp.so">Careers</NavLink>
                 </Disclosure.Button>
                 <Disclosure.Button>
-                  <Link href="#experience">
-                    <a className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
-                      Experience
-                    </a>
-                  </Link>
+                  <NavLink to="https://discord.gg/XH66BZFW4m">
+                    <Image
+                      src={discordLogo}
+                      alt="Discord Logo"
+                      width={18}
+                      height={18}
+                    />
+                    <span className="ml-2">Discord</span>
+                  </NavLink>
+                </Disclosure.Button>
+                <NavLink to="https://twitter.com/posp_so">
+                  <Image
+                    src={twitterLogo}
+                    alt="Twitter Logo"
+                    width={18}
+                    height={18}
+                  />
+                  <span className="ml-2">Twitter</span>
+                </NavLink>
+                <Disclosure.Button>
+                  <NavLink to="https://github.com/posp-so">
+                    <Image
+                      src={githubLogo}
+                      alt="GitHub Logo"
+                      width={18}
+                      height={18}
+                    />
+                    <span className="ml-2">GitHub</span>
+                  </NavLink>
                 </Disclosure.Button>
                 <Disclosure.Button>
-                  <Link href="#projects">
-                    <a className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
-                      Projects
-                    </a>
-                  </Link>
-                </Disclosure.Button>
-                <Disclosure.Button>
-                  <Link href="#contact">
-                    <a className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
-                      Contact
-                    </a>
-                  </Link>
-                </Disclosure.Button>
-                <Disclosure.Button>
-                  <Link href="/resume.pdf">
-                    <a className="rounded-md px-3 py-2 text-sm font-medium text-[#7851a9] outline outline-1 outline-[#7851a9] hover:bg-purple-100 hover:bg-opacity-10">
-                      Resume
+                  <Link href="https://app.posp.so">
+                    <a>
+                      <BrandButton>Go to app</BrandButton>
                     </a>
                   </Link>
                 </Disclosure.Button>
--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const NavLink: React.FC<{ to: string }> = ({ children, to }) => {
   return (
     <Link href={to}>
-      <a className="flex items-center justify-center rounded-md px-3 py-2 align-middle text-sm font-medium text-black hover:bg-gray-100 hover:bg-opacity-50">
+      <a className="flex items-center justify-center rounded-md p-2 align-middle text-sm font-medium text-black hover:bg-gray-100 hover:bg-opacity-50">
         {children}
       </a>
     </Link>
